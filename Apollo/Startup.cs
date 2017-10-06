7512278baeb2396d4977f3cceda4670507549bcd
P1311480 <P1311480@NCS-180817KK12>
2017-10-06 12:52:13 +0800

    Add project files.

---
+++ Apollo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Apollo.Startup))]
+namespace Apollo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
